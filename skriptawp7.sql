ce4966441817060f4d53c1066d938fd748ca70e8
LeCento95 <stojancaric8@gmail.com>
2024-11-27 19:17:26 +0100

    Osnovne naredba za drop ako postoji i kreiranje baze

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -1,6 +1,11 @@
-﻿-- create database edunovawp7;
+﻿use master;
+go
+drop database if exists edunovawp7;
 
--- use edunovawp7;
+create database edunovawp7;
+go
+use edunovawp7;
+go
 
 create table smjer(
 naziv varchar (50)

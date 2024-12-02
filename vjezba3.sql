e7cc41f68e05c008bd4f2bce03d4968c77108bac
LeCento95 <stojancaric8@gmail.com>
2024-12-02 19:50:23 +0100

    Revert "Delete vjezba3.sql"
    This reverts commit f4e2b2347aa4143734a270cdf28727428eb53837.

---
+++ vjezba3.sql
@@ -0,0 +1,21 @@
+﻿use master;
+go
+drop database if exists vjezba3;
+go
+create database vjezba3;
+go
+
+create table IspitniRok(
+sifra int,
+predmet varchar (50),
+trajanje int,
+izvodi_se_od datetime,
+pristupio bit,
+);
+
+create table Pristupnici (
+ispitni_rok int,
+student varchar (50),
+broj_bodova varchar (100),
+ocijena varchar (10)
+);

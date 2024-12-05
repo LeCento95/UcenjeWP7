3129bf61724f5952fc2c8319d25e65ba17b1c4da
Stojan Carić <stojancaric8@gmail.com>
2024-12-05 19:29:44 +0100

    Update Restorant_Manager_Reservation

--- Restorant_Manager_Reservation.sql
+++ Restorant_Manager_Reservation.sql
@@ -7,14 +7,6 @@ go
 use Restoran_Manager_Reservation;
 go
 
-create table restoran (
-sifra int primary key identity (1,1) not null,
-naziv varchar (50) not null,
-telefon varchar (20) not null,
-e_mail varchar (50) not null,
-adresa varchar (50) not null
-);
-
 create table gosti (
 sifra int primary key identity (1,1) not null,
 ime varchar (50) not null,
@@ -31,17 +23,8 @@ naziv varchar (50) not null
 
 create table stolovi (
 sifra int primary key identity (1,1) not null,
-restoran int references restoran(sifra) not null,
 kapacitet int not null,
 status bit
-);
-
-create table rezervacija (
-sifra int primary key identity (1,1) not null,
-datum date not null,
-vrijeme time not null,
-gosti int references gosti(sifra) not null,
-stolovi int references stolovi(sifra) not null
 );
 
 create table meni (
@@ -54,7 +37,18 @@ kategorije int references kategorije(sifra) not null
 create table narudzba (
 sifra int primary key identity (1,1) not null,
 kolicina int not null,
-rezervacija int references rezervacija(sifra) not null,
+gosti int references gosti(sifra) not null,
+rezervacija int references rezervacija (sifra) not null,
 meni int references meni(sifra) not null,
 status bit
+);
+
+create table rezervacija (
+sifra int primary key identity (1,1) not null,
+datum date not null,
+vrijeme time not null,
+meni int references meni(sifra) not null,
+gosti int references gosti(sifra) not null,
+stolovi int references stolovi(sifra) not null,
+narudzba int references narudzba(sifra) not null
 );

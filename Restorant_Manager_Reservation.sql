b5720addf07d0731a807cb6a9aa40e20d8cf1972
Stojan Carić <stojancaric8@gmail.com>
2024-12-05 17:04:40 +0100

    Ispravak

--- Restorant_Manager_Reservation.sql
+++ Restorant_Manager_Reservation.sql
@@ -7,11 +7,12 @@ go
 use Restoran_Manager_Reservation;
 go
 
-create table stolovi (
+create table restoran (
 sifra int primary key identity (1,1) not null,
-broj_stola int not null,
-kapacitet bit not null,
-status bit not null
+naziv varchar (50) not null,
+telefon varchar (20) not null,
+e_mail varchar (50) not null,
+adresa varchar (50) not null
 );
 
 create table gosti (
@@ -22,9 +23,38 @@ telefon varchar (20) not null,
 e_mail varchar (50) not null
 );
 
+create table kategorije (
+sifra int primary key identity (1,1) not null,
+naziv varchar (50) not null
+);
+
+
+create table stolovi (
+sifra int primary key identity (1,1) not null,
+restoran int references restoran(sifra) not null,
+kapacitet int not null,
+status bit
+);
+
+create table rezervacija (
+sifra int primary key identity (1,1) not null,
+datum date not null,
+vrijeme time not null,
+gosti int references gosti(sifra) not null,
+stolovi int references stolovi(sifra) not null
+);
+
 create table meni (
 sifra int primary key identity (1,1) not null,
 naziv_jela varchar (50) not null,
 cijena decimal (18,2) not null,
-kategorija varchar (50) not null
+kategorije int references kategorije(sifra) not null
+);
+
+create table narudzba (
+sifra int primary key identity (1,1) not null,
+kolicina int not null,
+rezervacija int references rezervacija(sifra) not null,
+meni int references meni(sifra) not null,
+status bit
 );

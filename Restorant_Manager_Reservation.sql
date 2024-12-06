9a8b334c8064e4c9d52567c3cde7cca5e9b3b16d
LeCento95 <stojancaric8@gmail.com>
2024-12-06 19:41:12 +0100

    Update Restorant_Manager_Reservation.sql

--- Restorant_Manager_Reservation.sql
+++ Restorant_Manager_Reservation.sql
@@ -15,12 +15,6 @@ telefon varchar (20) not null,
 e_mail varchar (50) not null
 );
 
-create table kategorije (
-sifra int primary key identity (1,1) not null,
-naziv varchar (50) not null
-);
-
-
 create table stolovi (
 sifra int primary key identity (1,1) not null,
 kapacitet int not null,
@@ -30,16 +24,7 @@ status bit
 create table meni (
 sifra int primary key identity (1,1) not null,
 naziv_jela varchar (50) not null,
-cijena decimal (18,2) not null,
-kategorije int references kategorije(sifra) not null
-);
-
-create table narudzba (
-sifra int primary key identity (1,1) not null,
-kolicina int not null,
-stolovi int references stolovi (sifra) not null,
-meni int references meni(sifra) not null,
-status bit
+cijena decimal (18,2) not null
 );
 
 create table rezervacija (
@@ -48,4 +33,12 @@ datum date not null,
 vrijeme time not null,
 gosti int references gosti(sifra) not null,
 stolovi int references stolovi(sifra) not null
+);
+
+create table narudzba (
+sifra int primary key identity (1,1) not null,
+kolicina int not null,
+rezervacija int references rezervacija(sifra) not null,
+meni int references meni(sifra) not null,
+status bit
 );

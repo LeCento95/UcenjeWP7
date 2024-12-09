a56c97e893338137c50679b30a98a02404ffea3d
Stojan Carić <stojancaric8@gmail.com>
2024-12-09 23:39:33 +0100

    Restorant_Manager_Reservation

--- Restorant_Manager_Reservation.sql
+++ Restorant_Manager_Reservation.sql
@@ -9,45 +9,56 @@ go
 
 create table stolovi (
 sifra int primary key identity (1,1) not null,
-kapacitet int,
-lokacija varchar (50)
+kapacitet int not null,
+lokacija varchar (50) not null
 );
 
 create table meni (
 sifra int primary key identity (1,1) not null,
-naziv_jela varchar (100),
-cijena decimal (10,2),
-kategorija varchar (50)
+naziv_jela varchar (100) not null,
+cijena decimal (10,2) not null,
+kategorija varchar (50) not null
 );
 
 create table rezervacije (
 sifra int primary key identity (1,1) not null,
 datum datetime not null,
 broj_osoba int not null,
-stol int not null references stolovi(sifra),
+stol int not null references meni(sifra),
 ime_gosta varchar (100) not null
 );
 
 create table narudzba (
 sifra int primary key identity (1,1) not null,
-rezervacija int not null references stolovi(sifra),
+rezervacija int not null references rezervacije(sifra),
 meni int not null references meni(sifra)
 );
 
---1
 select * from stolovi;
 insert into stolovi (kapacitet, lokacija)
-values (10, 'Kamin');
+values
+(2, 'A 1'),
+(4, 'A 2');
 
---2
 select * from meni;
 insert into meni (naziv_jela, cijena, kategorija)
-values ('Sarma', 9.00, 'Zimska jela');
+values
+('Pizza Slavonska', 14.00, 'Pizza'),
+('Sarma', 9.00, 'Zimska jela');
 
---3
-insert into rezervacije (datum_vrijeme, broj_osoba, stol, ime_gosta)
-values ('2024-12-09', 6, 10,'Tomislav Jakopec')
+select * from rezervacije;
+insert into rezervacije (datum, broj_osoba, stol, ime_gosta)
+values
+('2024-12-10', 4, 2, 'Tomislav Jakopec'),
+('2024-12-12', 2, 4, 'Nikola Nikolić');
 
---4
+select * from narudzba;
 insert into narudzba (rezervacija, meni)
-values ()
+values
+(1, 1), -- rezervacija 1, pizza slavonska
+(1, 3); -- rezervacija 1, sarma
+
+select * from stolovi;
+select * from meni;
+select * from rezervacije;
+select * from narudzba;

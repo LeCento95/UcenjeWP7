0dae112820d2d84d04eec1885fd87c13fe0660a6
mturalija <marinamiochr@gmail.com>
2024-12-09 19:42:10 +0100

    Update Restorant_Manager_Reservation

--- Restorant_Manager_Reservation.sql
+++ Restorant_Manager_Reservation.sql
@@ -8,44 +8,40 @@ use Restorant_Manager_Reservation;
 go
 
 create table Stolovi (
-stol_sifra int primary key identity (1,1) not null,
+sifra int primary key identity (1,1) not null,
 kapacitet int,
 lokacija varchar (50)
 );
 
 create table Meni (
-meni_sifra int primary key identity (1,1) not null,
+sifra int primary key identity (1,1) not null,
 jela varchar (100),
 cijena decimal (10,2),
 kategorija varchar (50)
 );
 
 create table Rezervacije (
-rezervacija_sifra int primary key identity (1,1) not null,
+sifra int primary key identity (1,1) not null,
 datum_vrijeme datetime,
 broj_osoba int,
 ime_gosta varchar (100),
-stol_sifra int,
-foreign key (stol_sifra) references stolovi(stol_sifra)
+stol int not null references stolovi(sifra)
 );
 
 create table Narudzba (
-narudzba_sifra int primary key identity (1,1) not null,
+sifra int primary key identity (1,1) not null,
 kolicina int,
-stol_sifra int,
-meni_sifra int,
-foreign key (stol_sifra) references stolovi(stol_sifra),
-foreign key (meni_sifra) references meni(meni_sifra)
+stol int not null references stolovi(sifra),
+meni int not null references Meni(sifra)
 );
-
-insert into Stolovi (stol_sifra, kapacitet, lokacija)
-values (11, 6, 'Kamin');
+insert into Stolovi (kapacitet, lokacija)
+values (10, 6, 'Kamin');
 
 insert into Meni (meni_sifra, cijena, kategorija)
 values ('Pica Slavonska', 12.00, 'Pizza');
 
 insert into Rezervacije (datum_vrijeme, broj_osoba, ime_gosta, stol_sifra)
-VALUES ('2024-12-12 19:00:00', 4, 'Tomislav Jakopec', 10);
+values ('2024-12-12 19:00:00', 4, 'Tomislav Jakopec', 10);
 
 insert into Narudzba (stol_sifra, meni_sifra, kolicina)
 select 10, meni_sifra, 2

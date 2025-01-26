c5a7240c8c3c513231bb535b34e89064c1b1b031
Stojan Carić <stojancaric8@gmail.com>
2025-01-26 18:25:44 +0100

    E13Z1. zadatci

--- CSHARP/Ucenje/E13Z1.cs
+++ CSHARP/Ucenje/E13Z1.cs
@@ -9,9 +9,77 @@ namespace Ucenje
     internal class E13Z1
     {
         // Osigurati unos broja
+        public static void Izvedi()
+        {
+            int broj;
+            while (true)
+            {
+                Console.WriteLine("Unesite cijeli broj: ");
+                string unos = Console.ReadLine();
+                try
+                {
+                    broj = int.Parse(unos);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Pogrešan unos. Molim unesite cijeli broj.");
+                }
+            }
+            Console.WriteLine($"Unijeli ste broj: {broj}");
 
-        // Osigurati unos parnog broja
+            // Osigurati unos parnog broja
 
-        // Osigurati unos visine čovjeka
+
+            int br;
+            while (true)
+            {
+                Console.WriteLine("Unesite parni cijeli broj: ");
+                string unos = Console.ReadLine();
+                try
+                {
+                    br = int.Parse(unos);
+                    if (br % 2 == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Broj nije paran. Molim unesite parni broj.");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Pogrešan unos. Molim unesite cijeli broj");
+                }
+            }
+            Console.WriteLine($"Unijeli ste parni broj: {br}");
+
+            // Osigurati unos visine čovjeka
+
+            double visina;
+            while (true)
+            {
+                Console.WriteLine("Unesite visinu čovjeka u metrima: ");
+                string unos = Console.ReadLine();
+                try
+                {
+                    visina = double.Parse(unos);
+                    if (visina > 0 && visina < 3)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nerealna visina za čovjeka. Unesi visinu u metrima.");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Pogrešan unos. Unesi broj u oblicu x.xx");
+                } 
+            }
+            Console.WriteLine($"Unio si visinu: {visina} m");
+        }
     }
 }
